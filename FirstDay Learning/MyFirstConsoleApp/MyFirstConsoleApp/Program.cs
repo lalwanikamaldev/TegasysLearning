2fb8ea9e3e5ca43453ec530062ab80327751fdff
lalwanikamaldev <65231431+lalwanikamaldev@users.noreply.github.com>
2023-07-13 11:50:49 +0530

    Update Program.cs

--- FirstDay Learning/MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
+++ FirstDay Learning/MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -43,3 +43,111 @@
         // if , if eles
     }
 }
+
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+// Mobile 
+
+// Iphone : Mobile => 
+
+// Android =>  Samsung , Redmi 
+
+
+
+
+// class is a blueprint of object 
+
+// object of Bike/activa/car/ (Create a bike)
+
+public class Vehicle 
+{
+    
+    public string Color {get;set;}
+    
+    public int EngineNumber {get;set;}
+    
+    public int Speed {get;set;}
+    
+    public void Accelerate()
+    {
+        // Accelerate , car bike 
+    }
+    
+   
+    //
+    public void start()
+    {
+        
+    }
+    
+    // function 
+    public void stop()
+    {
+        
+    }
+}
+}
+
+class Activa : Vehicle
+{
+    
+}
+
+class Car : Vehicle 
+{
+    public string Color {get;set;}
+    
+    public int EngineNumber {get;set;}
+    
+    public int Speed {get;set;}
+    
+    public void Accelerate()
+    {
+        // Accelerate , car bike 
+    }
+    
+   
+    //
+    public void start()
+    {
+        
+    }
+    
+    // function 
+    public void stop()
+    {
+        
+    }
+    public void GearChanger()
+    {
+        
+    }
+    
+    public void Abs()
+    {
+        
+    }
+    
+    public void HandBreak()
+    {
+        
+    }
+    
+    public void CloseOpenDoor()
+    {
+    }
+     
+}
+
+
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Hello Mono World");
+    }
+}
