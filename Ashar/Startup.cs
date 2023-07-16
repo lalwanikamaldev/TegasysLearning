6f4b94c983e0e828645d5827a84058fe11c9798d
AsharRQazi <135799480+AsharRQazi@users.noreply.github.com>
2023-07-16 21:21:21 +0530

    Add files via upload

---
+++ Ashar/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Ling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject
+{
+    public class Mobile
+    {
+        public int RAM { get; set; }
+        public int ROM { get; set; }
+
+        public string Color { get; set; }
+
+        public string Manufacturer { get; set; }
+
+
+        public void Price()
+        {
+
+        }
+
+    }
+
+    public class Android : Phone
+    {
+        static void Main()
+        {
+
+            Android 13 = Android();
+        }
+
+    }
+
+
+    public class Windows : Phone
+    {
+        static void Main()
+        { 
+        Windows 8 = Windows();
+    }
+}
+
+     
+    public class iPhone : iPhon
+    { 
+
+       static void Main()
+    {
+            iPhone 14 = iPhone();
+
+        }
+    }
+
+
+
+
