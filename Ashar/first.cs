885b0b922b4c66c49b9793b76d7e3c5412f40646
AsharRQazi <135799480+AsharRQazi@users.noreply.github.com>
2023-07-17 16:06:42 +0530

    Add files via upload

---
+++ Ashar/first.cs
@@ -0,0 +1,5 @@
+﻿Build started...
+1>------ Build started: Project: MyLearning, Configuration: Debug Any CPU ------
+1>D:\MyLearnings\MyLearning\Program.cs(66,6,66,6): error CS1513: } expected
+1>Done building project "MyLearning.csproj" -- FAILED.
+========== Build: 0 succeeded, 1 failed, 0 up-to-date, 0 skipped ==========
