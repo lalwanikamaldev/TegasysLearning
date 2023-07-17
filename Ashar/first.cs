9d35f0f5c7e81e6c72f702959e6e7e15a382d302
AsharRQazi <135799480+AsharRQazi@users.noreply.github.com>
2023-07-17 16:10:14 +0530

    Delete first.cs
    unapropriate file

--- Ashar/first.cs
+++
@@ -1,5 +0,0 @@
-﻿Build started...
-1>------ Build started: Project: MyLearning, Configuration: Debug Any CPU ------
-1>D:\MyLearnings\MyLearning\Program.cs(66,6,66,6): error CS1513: } expected
-1>Done building project "MyLearning.csproj" -- FAILED.
-========== Build: 0 succeeded, 1 failed, 0 up-to-date, 0 skipped ==========
