5a02d12766a473d7ee1c265b389250628ee30ddd
lalwanikamaldev <65231431+lalwanikamaldev@users.noreply.github.com>
2023-07-24 15:56:46 +0530

    Update Bank.cs

--- Ajit/ClassImplementation/Bank.cs
+++ Ajit/ClassImplementation/Bank.cs
@@ -12,7 +12,7 @@ namespace ClassImplementation
         protected int IFSC; //As this is proctected, IFSC can be used in Customer class also
         internal bool LockerAvailability; //As this is internal, it can be used within this assembly only
         
-        //Parameterless Construtor
+        //default Construtor 
         public Bank()
         {
             this.Country = "India";
@@ -28,14 +28,15 @@ namespace ClassImplementation
     public class Customer:Bank
     {
         //ByDefault private so no need to use private keyword explicitly
-        int _CustomerID, _AvailableBalance;
+        private int readonly _CustomerID, _AvailableBalance;
         string _CustomerName;
         bool _AccountStatus;
 
         //Parameterized Constructor
         public Customer(int CustomerID, int AvailableBalance, string CustomerName, bool AccountStatus)
         {
-            CustomerID = _CustomerID;
+           int a= 5;
+           _CustomerID = CustomerID;
             AvailableBalance = _AvailableBalance;
             CustomerName = _CustomerName;
             AccountStatus = _AccountStatus;
@@ -68,4 +69,77 @@ namespace ClassImplementation
 
 
 
+}
+
+public class EngineClass {
+ public virtual void TansformFuelToEnergy()
+ {
+  Console.WriteLine("I am in Engine class ");
+  // engine related transformation generic (take fule and transform 
+ }
+
+ public void SmokeRelease()
+ {
+ // implementaton 
+ 
+}
+}
+
+public class BikeEngine : EngineClass  {
+// public void SmokeRelease()
+// { // inharitance 
+ // implementaton 
+ 
+//}
+
+ public override void TansformFuelToEnergy(int sa)
+ {
+ // logic which differ for BikeEngine 
+   Console.WriteLine("I am in BikeEngine class ");
+}
+}
+
+public class ActivaEngine :  EngineClass {
+ public override void TansformFuelToEnergy(int sa)
+ {
+ // logic which differ for BikeEngine 
+   Console.WriteLine("I am in BikeEngine class ");
+}
+}
+
+public class Bike {
+private readonly EngineClass _engine;
+
+public Bike(EngineClass engineClassObject)
+{
+_engine = engineClassObject;  //myactiva
+}
+// anywhere 
+_engine => myactiva .
+
+
+}
+
+public class Activata {
+public BikeEngine Engine {get;set;}
+}
+
+main()
+{
+BikeEngine mybike = new BikeEngine();
+mybike.TansformFuelToEnergy(); // what output it is giving 
+EngineClass engine = new BikeEngine();
+engine.TansformFuelToEnergy(); // that output  it is giving 
+ActivaEngine myActiva = new ActivaEngine();
+mybike.TansformFuelToEnergy(); // what output it is giving 
+
+Bike bike = new Bike(myActiva);
+bike myBike = new Bike(mybike);
+
+
+public Customer(int CustomerID, int AvailableBalance, string CustomerName, bool AccountStatus)
+
+Customer customer = new Customer(33,346,"Kamal", true);
+    
+    
 }
